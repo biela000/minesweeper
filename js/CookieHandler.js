3185e2f081d3a0c99eab5ec7f350c522c88fab91
biela000 <jak.biela000@gmail.com>
2022-10-18 08:30:07 +0200

    I guess finish almost

--- js/CookieHandler.js
+++ js/CookieHandler.js
@@ -1,70 +1,67 @@
 export default class CookieHandler {
-    static #chosenCookieName = "minesweeperRecords";
+  static #chosenCookieName = "minesweeperRecords";
 
-    static #getCookie(wantedCookieName) {
-        const splitCookies = document.cookie.split(";");
-        for (const cookie of splitCookies) {
-            const cookieName = cookie.substring(0, cookie.indexOf("="));
-            const cookieValue = cookie.substring(cookie.indexOf("=") + 1);
-            if (cookieName === wantedCookieName) {
-                return JSON.parse(cookieValue);
-            }
-        }
+  static #getCookie(wantedCookieName) {
+    const splitCookies = document.cookie.split(";");
+    for (const cookie of splitCookies) {
+      const cookieName = cookie.substring(0, cookie.indexOf("="));
+      const cookieValue = cookie.substring(cookie.indexOf("=") + 1);
+      if (cookieName === wantedCookieName) {
+        return JSON.parse(cookieValue);
+      }
     }
+  }
 
-    static get(wantedAmount, sortFunc) {
-        try {
-            console.log(sortFunc);
-            const cookieRecords = this.#getCookie(this.#chosenCookieName);
-            console.log(cookieRecords);
-            const preparedCookieRecords = cookieRecords
-                .filter((_, ind) => ind < wantedAmount)
-                .sort(sortFunc);
-            return {
-                status: "success",
-                data: {
-                    records: preparedCookieRecords,
-                },
-            };
-        } catch (err) {
-            return {
-                status: "error",
-                message: err.message,
-            };
-        }
+  static get(wantedAmount, sortFunc) {
+    try {
+      const cookieRecords = this.#getCookie(this.#chosenCookieName);
+      const preparedCookieRecords = cookieRecords
+        .filter((_, ind) => ind < wantedAmount)
+        .sort(sortFunc);
+      return {
+        status: "success",
+        data: {
+          records: preparedCookieRecords,
+        },
+      };
+    } catch (err) {
+      return {
+        status: "error",
+        message: err.message,
+      };
     }
+  }
 
-    static post(record) {
-        try {
-            const cookieRecords = this.#getCookie(this.#chosenCookieName) || [];
-            cookieRecords.push(record);
-            let splitCookies = document.cookie.split(";");
-            let preparedCookies = "";
+  static post(record) {
+    try {
+      const cookieRecords = this.#getCookie(this.#chosenCookieName) || [];
+      cookieRecords.push(record);
+      let splitCookies = document.cookie.split(";");
+      let preparedCookies = "";
 
-            if (splitCookies.length > 1) {
-                preparedCookies = splitCookies
-                    .filter((cookie) => {
-                        cookie.substring(0, cookie.indexOf("=")) !==
-                            this.#chosenCookieName;
-                    })
-                    .join();
-            }
+      if (splitCookies.length > 1) {
+        preparedCookies = splitCookies
+          .filter((cookie) => {
+            cookie.substring(0, cookie.indexOf("=")) !== this.#chosenCookieName;
+          })
+          .join();
+      }
 
-            document.cookie =
-                preparedCookies +
-                `${this.#chosenCookieName}=${JSON.stringify(cookieRecords)};`;
+      document.cookie =
+        preparedCookies +
+        `${this.#chosenCookieName}=${JSON.stringify(cookieRecords)};`;
 
-            return {
-                status: "success",
-                data: {
-                    record,
-                },
-            };
-        } catch (err) {
-            return {
-                status: "error",
-                message: err.message,
-            };
-        }
+      return {
+        status: "success",
+        data: {
+          record,
+        },
+      };
+    } catch (err) {
+      return {
+        status: "error",
+        message: err.message,
+      };
     }
+  }
 }
--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,80 +1,78 @@
 import CookieHandler from "./CookieHandler.js";
 
 class Scoreboard {
-    #descSort(a, b) {
-        if (a.score <= b.score) {
-            return 1;
-        } else {
-            return -1;
-        }
+  #descSort(a, b) {
+    if (a.score <= b.score) {
+      return 1;
+    } else {
+      return -1;
     }
+  }
 
-    #ascSort(a, b) {
-        if (a.score >= b.score) {
-            return 1;
-        } else {
-            return -1;
-        }
+  #ascSort(a, b) {
+    if (a.score >= b.score) {
+      return 1;
+    } else {
+      return -1;
     }
+  }
 
-    constructor(amount, sort, DOMContainer, DOMInputs) {
-        this.amount = amount;
-        this.sort = sort === "DESC" ? this.#descSort : this.#ascSort;
+  constructor(amount, sort, DOMContainer, DOMInputs) {
+    this.amount = amount;
+    this.sort = sort === "DESC" ? this.#descSort : this.#ascSort;
 
-        this.records = CookieHandler.get(this.amount, this.sort).data.records;
-        this.DOMContainer = DOMContainer;
+    this.records = CookieHandler.get(this.amount, this.sort).data.records;
+    this.DOMContainer = DOMContainer;
 
-        this.DOMInputs = DOMInputs;
-        this.DOMInputs.forEach((input) => {
-            if (input.name === "amount") {
-                this.records.forEach((_, ind) => {
-                    const tmpOption = document.createElement("option");
-                    tmpOption.value = ind + 1;
-                    tmpOption.innerText = ind + 1;
-                    input.appendChild(tmpOption);
-                });
-                this.amount =
-                    this.amount > this.records.length
-                        ? this.records.length
-                        : this.amount;
-                input.value = this.amount;
-            }
-            input.addEventListener("change", () => {
-                this.changeProperty(input.name, input.value);
-            });
+    this.DOMInputs = DOMInputs;
+    this.DOMInputs.forEach((input) => {
+      if (input.name === "amount") {
+        this.records.forEach((_, ind) => {
+          const tmpOption = document.createElement("option");
+          tmpOption.value = ind + 1;
+          tmpOption.innerText = ind + 1;
+          input.appendChild(tmpOption);
         });
+        this.amount =
+          this.amount > this.records.length ? this.records.length : this.amount;
+        input.value = this.amount;
+      }
+      input.addEventListener("change", () => {
+        this.changeProperty(input.name, input.value);
+      });
+    });
 
-        this.createDOMRecords();
+    this.createDOMRecords();
+  }
+
+  changeProperty(name, value) {
+    if (name === "sort") {
+      this.sort = value === "DESC" ? this.#descSort : this.#ascSort;
+    } else {
+      this.amount = +value;
     }
 
-    changeProperty(name, value) {
-        if (name === "sort") {
-            this.sort = value === "DESC" ? this.#descSort : this.#ascSort;
-        } else {
-            this.amount = +value;
-        }
+    this.records = CookieHandler.get(this.amount, this.sort).data.records;
+    this.clearDOMRecords();
+    this.createDOMRecords();
+  }
 
-        this.records = CookieHandler.get(this.amount, this.sort).data.records;
-        this.clearDOMRecords();
-        this.createDOMRecords();
-    }
+  clearDOMRecords() {
+    this.DOMContainer.innerHTML = "";
+  }
 
-    clearDOMRecords() {
-        this.DOMContainer.innerHTML = "";
-    }
-
-    createDOMRecords() {
-        this.records.forEach((record, ind) => {
-            const tmpElement = document.createElement("li");
-            tmpElement.classList.add("scoreboard-record");
-            tmpElement.innerHTML = `<div class=\"id\">${ind + 1}</div>`;
-            tmpElement.innerHTML += `<div class=\"box-count\">${record.boxCount}</div>`;
-            tmpElement.innerHTML += `<div class=\"mine-count\">${record.mineCount}</div>`;
-            tmpElement.innerHTML += `<div class=\"time\">${record.time}</div>`;
-            tmpElement.innerHTML += `<div class=\"score\">${record.score}</div>`;
-            this.DOMContainer.appendChild(tmpElement);
-        });
-    }
+  createDOMRecords() {
+    this.records.forEach((record) => {
+      const tmpElement = document.createElement("li");
+      tmpElement.classList.add("scoreboard-record");
+      tmpElement.innerHTML = `<div class=\"username\">${record.username}</div>`;
+      tmpElement.innerHTML += `<div class=\"box-count\">${record.boxCount}</div>`;
+      tmpElement.innerHTML += `<div class=\"mine-count\">${record.mineCount}</div>`;
+      tmpElement.innerHTML += `<div class=\"time\">${record.time}</div>`;
+      tmpElement.innerHTML += `<div class=\"score\">${record.score}</div>`;
+      this.DOMContainer.appendChild(tmpElement);
+    });
+  }
 }
 
 const scoreboardContainer = document.querySelector(".scoreboard");
